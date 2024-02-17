6dc4f79d2ba9ae64fd9614d0eae51e14bb108189
Sergey Dyatlov <batman_superman@list.ru>
2024-02-17 20:32:44 +0400

    module2-task1 fix errors

--- src/app/app.tsx
+++
@@ -1,6 +0,0 @@
-import { mockData } from '../mock-data/mock-data';
-import { MainPage } from '../pages/main-page';
-
-export const App = (): JSX.Element => (
-  <MainPage countOffers={mockData.length}/>
-);
